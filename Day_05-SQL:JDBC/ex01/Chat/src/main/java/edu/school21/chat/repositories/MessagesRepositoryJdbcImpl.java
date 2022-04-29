ba2fc250b762559f6c15bf46b8f7cb13c1c9711f
Дарья Паркина <lkasandr@student.21-school.ru>
2022-04-29 17:04:25 +0300

    change try_with_resources

--- Day_05-SQL:JDBC/ex01/Chat/src/main/java/edu/school21/chat/repositories/MessagesRepositoryJdbcImpl.java
+++ Day_05-SQL:JDBC/ex01/Chat/src/main/java/edu/school21/chat/repositories/MessagesRepositoryJdbcImpl.java
@@ -21,10 +21,10 @@ public class MessagesRepositoryJdbcImpl implements MessagesRepository{
 
     public Optional<Message> findById(Long id) {
         Optional<Message> optionalMessage = null;
-        try {
-            Connection cM = ds.getConnection();
-            Statement sM = cM.createStatement();
-            ResultSet rsM = sM.executeQuery("SELECT * FROM \"Message\" WHERE messageid = " + id);
+        try (Connection cM = ds.getConnection();
+             Statement sM = cM.createStatement();
+             ResultSet rsM = sM.executeQuery("SELECT * FROM \"Message\" WHERE messageid = " + id))
+        {
             rsM.next();
             setUser(rsM.getLong("userid"));
             setChatroom(rsM.getLong("roomid"));
@@ -36,9 +36,6 @@ public class MessagesRepositoryJdbcImpl implements MessagesRepository{
                     rsM.getTimestamp("datemessage").toLocalDateTime()));
             this.message = optionalMessage.get();
             printInfo();
-            rsM.close();
-            sM.close();
-            cM.close();
 
         } catch (SQLException e) {
             System.out.println("No message");
@@ -48,34 +45,37 @@ public class MessagesRepositoryJdbcImpl implements MessagesRepository{
 
     private void setUser(long id) throws SQLException
     {
-        Connection cU = ds.getConnection();
-        Statement sU = cU.createStatement();
-        ResultSet rsU = sU.executeQuery("SELECT * FROM \"Users\" " + "WHERE userid = " + id);
-        rsU.next();
-        Optional<User> optionalUser = Optional.of(new User(
-                rsU.getLong("userid"),
-                rsU.getString("login"),
-                rsU.getString("password")));
-        this.user = optionalUser.get();
-        rsU.close();
-        sU.close();
-        cU.close();
+        try(Connection cU = ds.getConnection();
+            Statement sU = cU.createStatement();
+            ResultSet rsU = sU.executeQuery("SELECT * FROM \"Users\" " + "WHERE userid = " + id))
+        {
+            rsU.next();
+            Optional<User> optionalUser = Optional.of(new User(
+                    rsU.getLong("userid"),
+                    rsU.getString("login"),
+                    rsU.getString("password")));
+            this.user = optionalUser.get();
+        }
+        catch (SQLException e) {
+            System.out.println("No message");
+        }
     }
 
     private void setChatroom(long id) throws SQLException
     {
-        Connection cR = ds.getConnection();
-        Statement sR = cR.createStatement();
-        ResultSet rsR = sR.executeQuery("SELECT * FROM \"Chatroom\" " +
-                "WHERE roomid = " + id);
-        rsR.next();
-        Optional<Chatroom> optionalChatroom = Optional.of( new Chatroom(
-                rsR.getLong("roomid"),
-                rsR.getString("roomname")));
-        this.room = optionalChatroom.get();
-        rsR.close();
-        sR.close();
-        cR.close();
+        try (Connection cR = ds.getConnection();
+             Statement sR = cR.createStatement();
+             ResultSet rsR = sR.executeQuery("SELECT * FROM \"Chatroom\" " +
+                     "WHERE roomid = " + id)) {
+            rsR.next();
+            Optional<Chatroom> optionalChatroom = Optional.of(new Chatroom(
+                    rsR.getLong("roomid"),
+                    rsR.getString("roomname")));
+            this.room = optionalChatroom.get();
+        }
+        catch (SQLException e) {
+            System.out.println("No message");
+        }
     }
 
     private void printInfo()
